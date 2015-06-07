feb97d1a7eec6f77651c600f83b190d039d5b686
hongyangAndroid <lmj623565791@gmail.com>
2015-06-07 23:41:28 +0800

    add blogcodes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Android_Blog_Demos
-source code in blog~
+
+source code in blog ~
+
+###[2015-06-07更新]
+
+以后所有的blog代码将以列表的形式添加到blogcodes这个module中。
+
+如下：
+
+<img src="screenshot.gif" width="320px"/>
+
+哈，列表Item会不断增加的。
+
+大家只要安装这一个，就可以查看所有博客上的源码示例，以前的几个modules保持不变。
---
+++ blogcodes/src/main/java/com/zhy/blogcodes/CategoryActivity.java
@@ -0,0 +1,69 @@
+package com.zhy.blogcodes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class CategoryActivity extends ActionBarActivity
+{
+    private ListView mListView;
+
+    private LayoutInflater mInflater;
+
+
+    private Class[] CLAZZES = new Class[]
+            {
+                    NavigationViewActivity.class,
+                    NavListViewActivity.class,
+            };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        mInflater = LayoutInflater.from(this);
+
+        mListView = (ListView) findViewById(R.id.id_listview);
+
+        mListView.setAdapter(new ArrayAdapter<Class>(this, -1, CLAZZES)
+                             {
+                                 @Override
+                                 public View getView(int position, View convertView, ViewGroup parent)
+                                 {
+                                     String title = getItem(position).getSimpleName();
+                                     if (convertView == null)
+                                     {
+                                         convertView = mInflater.inflate(R.layout.item_category, parent, false);
+                                     }
+                                     TextView tv = (TextView) convertView.findViewById(R.id.id_title);
+                                     tv.setText(title);
+                                     return convertView;
+                                 }
+                             }
+
+        );
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Intent intent = new Intent(CategoryActivity.this, CLAZZES[position]);
+                intent.putExtra(BaseContentActivity.TITLE, CLAZZES[position].getSimpleName());
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
