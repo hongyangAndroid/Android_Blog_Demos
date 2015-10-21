8d13654311300244e9ebd2b9fa773493e3d854b8
hongyangAndroid <lmj623565791@gmail.com>
2015-10-21 11:02:23 +0800

    调整结构

--- blogcodes/src/main/java/com/zhy/blogcodes/jni/JniTest.java
+++
@@ -1,9 +0,0 @@
-package com.zhy.blogcodes.jni;
-
-public class JniTest {
-
-	static {
-		System.loadLibrary("hellojni");
-	}
-	public native String welcome(String str);
-}
--- blogcodes/src/main/java/com/zhy/blogcodes/mvp/biz/IUserBiz.java
+++
@@ -1,9 +0,0 @@
-package com.zhy.blogcodes.mvp.biz;
-
-/**
- * Created by zhy on 15/6/19.
- */
-public interface IUserBiz
-{
-    public void login(String username, String password, OnLoginListener loginListener);
-}
